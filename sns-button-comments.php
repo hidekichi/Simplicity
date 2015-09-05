0b506bf196d906db3444fd9923db8bbde188aa1e
hidekichi <hidekichi0513@gmail.com>
2015-09-05 18:38:53 +0900

    Under construction...

--- sns-button-comments.php
+++ sns-button-comments.php
@@ -1,22 +1,30 @@
 <?php
-	if ( is_comments_btn_visible() && is_single() )://コメントボタンを表示するか
+	/**
+	 * コメントボタンを表示するか
+	 */
+
+	if ( is_comments_btn_visible() && is_single() ):
 
 	//コメントがある場合は、コメント一覧に、ない場合はコメント投稿欄に飛ばすためのアンカー
 	$comment_count_anchor = ( get_comments_number() > 0 ) ? '#comments' : '#reply-title';
 ?>
-<?php /*<li class="balloon-btn comments-balloon-btn">*/ ?>
+
+<li class="balloon-btn comments-balloon-btn">
+
 <li class="comments">
-	<?php /*<span class="balloon-btn-set">*/ ?>
+
+	<span class="balloon-btn-set">
 		<a href="#reply-title" rel="nofollow">
 			<span class="icon">
 				<span class="fa fa-comment"></span>
 			</span>
 		</a>
-		<?php /*<span class="arrow-box">*/ ?>
-		<a href="<?php echo $comment_count_anchor; ?>" rel="nofollow">
-			<span class="social-count comments-count"><?php echo get_comments_number(); ?></span>
-		</a>
-		<?php /*</span>*/ ?>
-	<?php /*</span>*/?>
+		<span class="arrow-box">
+			<a href="<?php echo $comment_count_anchor; ?>" rel="nofollow">
+				<span class="social-count comments-count"><?php echo get_comments_number(); ?></span>
+			</a>
+		</span>
+	</span>
+
 </li>
 <?php endif; //is_comments_btn_visible?>
