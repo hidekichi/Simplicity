176d53f6ff76955844e2a3febc479141840533e7
hidekichi <hidekichi0513@gmail.com>
2015-09-09 00:49:08 +0900

    20150909 new

---
+++ original_fix_files/scrollbar_scroll.js
@@ -0,0 +1,119 @@
+(function($){
+	/**
+	 *
+	 * follow scrool widget, wordpress theme Simplicty専用
+	 * ※ 使用時には助長なコメントは削除して下さい。説明用です。
+	 *
+	 * 2015/09/09 new
+	 *
+	 */
+
+	// オフセットを調べる関数
+	function offsetCheck(tgt){
+		return tgt.get( 0 ).offsetTop;
+	}
+
+	// heightを調べる関数
+	function heightCheck(tgt){
+		return tgt.height();
+	}
+
+	$(function(){
+
+		//setting
+		var mainHeight    = $("#main").height(),
+		    sidebarWidth  = $("#sidebar").width(),
+		    ft            = $("footer"),
+		    ftTop         = ft.get( 0 ).offsetTop,
+		    followArea    = $("#sidebar-scroll"),
+		    flwAreaHeight = followArea.outerHeight(),
+		    flwAreaTop    = followArea.get( 0 ).offsetTop,
+		    flwAreaBottom = flwAreaTop + flwAreaHeight;
+
+		/**
+		 * setTimeoutにて、0.2秒後に処理が行われるようにするためのtimerです
+		 * これがなければスクロールの最中も処理が行われるので、それを回避するため設置
+		 */
+		var timer = false;
+
+		/**
+		 * window.innerWidth > 1050 は、スクロール追従する処理を行う画面幅です
+		 * 1050px以上の画面幅がある時のみ処理を行います
+		 * wordpressローカル変数で、タブレットの時にデスクトップ表示の有無をチェックしても良いかも知れません
+		 */
+
+		if (window.innerWidth > 1050){
+
+			if (timer !== false ) { clearTimeout(timer); }
+
+			timer = setTimeout(function(){
+
+				$(window).on("scroll", function(){
+
+					var st = $(window).scrollTop();
+
+					if ( mainHeight > flwAreaBottom ){
+
+						// scrollTopが追従エリアのoffsetTopを超えた時
+						if ( st > flwAreaTop ){
+
+							/**
+							 * #条件
+							 *
+							 * scrollTopが、( footer.offset().top - #sidebar-scroll.offset().top )を超えた時か
+							 * [check]footer.offset().top - ( [check]追従エリア + footer上のマージン )よりscrollTopが大きい時
+							 *
+							 * これは、footerに追従エリアが到達して上に戻る時にfooterに追従エリアを張り付かせるためのものです
+							 * checkはスクリプト先頭の関数からその時のオフセット値を求めてます
+							 * スクロールごとにチェックするのでやり過ぎですが、追従エリアのサイズが変わった後、
+							 * 追従エリアがfooterを突き抜けることがあったため導入
+							 */
+
+							if (ftTop - flwAreaHeight < st || st > offsetCheck(ft) - (heightCheck(followArea) + 60) ) {
+
+								followArea.css({
+									position: "absolute",
+									width: sidebarWidth,
+									top: offsetCheck(ft) - ( heightCheck(followArea) + 60)
+								});
+
+							} else {
+
+								/**
+								 * footerに追従エリアが到達し、追従エリアの高さを超えるということは
+								 * 画面上辺に追従エリアが達したと言うことなのでfixedにしてtop:0になってます
+								 */
+
+								followArea.css({
+									position:"fixed",
+									width: sidebarWidth,
+									top: 0
+								});
+
+							}
+
+						} else {
+
+							/**
+							 * 追従エリアが元の座標に戻った場合は、jQueryのcssでstyleに追加したプロパティが不要なので
+							 * styleごと削除して元に戻します。
+							 * オリジナルではposition: staticでしたが、何もなければ基本staticなので
+							 * 特別に何も処理してません
+							 */
+
+							followArea.removeAttr("style");
+						}
+
+					} else {
+						/**
+						 * もしメインの記事の高さがサイドバーよりも低い場合は、ここに何か書くことで
+						 * スクロールに伴う独自の処理を行えます
+						 */
+					}
+
+				});
+
+			},200); //settimeoutの時間 1000 = 1秒
+		}
+	});
+})(jQuery);
