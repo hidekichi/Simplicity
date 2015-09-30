809ab8005ed26fdf430203a9d71e78e92731dd4e
hidekichi <hidekichi0513@gmail.com>
2015-09-30 09:07:15 +0900

    sns-count jQuery need this

---
+++ original_fix_files/blazechariot/lib/pocket-count.php
@@ -0,0 +1,21 @@
+<?php
+
+$url = $_GET['url'];
+$html = 'http://widgets.getpocket.com/v1/button?label=pocket&count=vertical&v=1&url=' . rawurlencode( $url );
+
+$ch = curl_init(); // 初期化
+curl_setopt( $ch, CURLOPT_URL, $html ); // URLの設定
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true ); // 出力内容を受け取る設定
+$result = curl_exec( $ch ); // データの取得
+curl_close($ch); // cURLのクローズ
+
+$domDocument = new DOMDocument('1.0', 'UTF-8');
+$domDocument->preserveWhiteSpace = false;
+$domDocument->loadHTML($result);
+$xmlString = $domDocument->saveXML();
+$xmlObject = simplexml_load_string($xmlString);
+$array = json_decode(json_encode($xmlObject), true);
+
+echo $array['body']['div']['a']['span']['em'];
+
+?>
