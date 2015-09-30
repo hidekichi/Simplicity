b66b3af78bdd0a1a93be2c460541efd27d519fb9
hidekichi <hidekichi0513@gmail.com>
2015-09-30 10:04:33 +0900

    sns-count jQuery need

--- original_fix_files/blazechariot/lib/hatena-count.php
+++ original_fix_files/blazechariot/lib/hatena-count.php
@@ -2,10 +2,10 @@
 
 $g_url = $_GET['url'];
 $api = 'http://api.b.st-hatena.com/entry.count?url=';
-$url = rawurlencode($g_url);
+$url = urlencode($g_url);
 
 $ch = curl_init(); // 初期化
-curl_setopt( $ch, CURLOPT_URL, $api.url ); // URLの設定
+curl_setopt( $ch, CURLOPT_URL, $api.$url ); // URLの設定
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true ); // 出力内容を受け取る設定
 $result = curl_exec( $ch ); // データの取得
 curl_close($ch); // cURLのクローズ
